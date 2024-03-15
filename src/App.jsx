0cd368f7b6f468d38e793ce51ad793bc08cbace3
Mostary Jahan <mostaryjahan01@gmail.com>
2024-03-15 23:45:12 +0600

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,60 @@
 
+import { useEffect } from 'react'
 import './App.css'
+import Header from './components/Header'
+import MainCard from './MainCard'
+import { useState } from 'react'
 
 function App() {
+  const [card, setCard] = useState([])
+
+  useEffect(() => {
+    fetch('./fakeData.json')
+      .then(res => res.json())
+      .then(data => {
+        setCard(data);
+      })
+  }
+
+    , [])
+  console.log(card);
 
   return (
     <>
-     
-     <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-center text-neutral-content">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
-    </div>
-  </div>
-</div>
-    
-     
+
+      <Header></Header>
+
+      <div className='container mx-auto'>
+        <div className='mt-8'>
+          <h3 className='text-3xl mt-8 text-center font-bold'>Our Recipes</h3>
+          <p className='font-normal text-center'>From beginners to seasoned cooks, our classes cater to all skill levels. Join us for an unforgettable culinary experience. Embark on a flavorful journey <br /> and master the art of cooking with our interactive and engaging classes</p>
+        </div>
+
+        <div className='flex justify-between mt-8'>
+          <div className='w-2/3 gap-4 lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 '>
+          {
+          card.map(pd => <MainCard card={pd}
+          ></MainCard>)
+        }
+          </div>
+       
+
+
+        <div className="border-2 rounded-2xl w-1/3">
+          <h3 className='text-center text-2xl font-bold mt-6'>Want to Cook</h3>
+          <hr />
+          
+        </div>
+        </div>
+       
+      </div>
+
+
+
+
+
+
+
     </>
   )
 }
